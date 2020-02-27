be2aa777fce91d9a39e1fe4237471e2532d34a5c
Artem Omukhov <a.omukhov@gmail.com>
2020-02-27 23:05:39 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>5*</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+</html>
+
+<?php
+
+echo ('Hello'); ?>
+
+</body>
